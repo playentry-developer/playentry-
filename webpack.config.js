fc7f5f6442c67952fc88032dd08f0e942a74ecc9
playentry-developer <cherrymango66@gmail.com>
2024-12-13 16:39:26 +0900

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack_config/common');
+
+let conf;
+
+switch (process.env.NODE_ENV) {
+    case 'lint':
+        // common config 을 가지지 않는다.
+        return require('./webpack_config/lint');
+    case 'production':
+        conf = require('./webpack_config/prod');
+        break;
+    case 'serve':
+    case 'serve-mini':
+        conf = require('./webpack_config/serve');
+        break;
+    case 'development':
+    default:
+        conf = require('./webpack_config/dev');
+        break;
+}
+
+module.exports = merge(common, conf);
